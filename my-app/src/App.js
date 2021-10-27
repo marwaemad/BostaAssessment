6626edbece6a7699649d4641555f0411ac7228b7
marwaemad <marwa@wuilt.com>
2021-10-27 20:08:08 +0200

    create bosta app

---
+++ my-app/src/App.js
@@ -0,0 +1,27 @@
+// import logo from "./logo.svg";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="container">
+          <div className="logo">
+            <img src="https://bosta.co/wp-content/uploads/2019/08/bosta_logo_en_red.svg" />
+          </div>
+          <div className="nav-container">
+            <nav>
+              <ul>
+                <li>home</li>
+                <li>pricing</li>
+                <li>contact sales</li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
